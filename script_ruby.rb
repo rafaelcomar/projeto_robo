d6488dd5548670a4efc4502597acb539c98506e1
rafaelcomar <rafael.comar93@gmail.com>
2015-06-30 14:52:26 -0300

    adicionando chromewebdriver e outras paginas do anuncio

--- script_ruby.rb
+++ script_ruby.rb
@@ -10,23 +10,31 @@ class CH
   def initialize
 
     http = Net::HTTP.new(@host, @port)
-    http.read_timeout = 50000000
+    http.read_timeout = 9000000
 
     # Configura e inicializa o navegador Firefox
     Watir::always_locate = false
-    @b = Watir::Browser.new :firefox
+
+   puts "inside hooks in before"
+    # profile = Selenium::WebDriver::Chrome::Profile.new
+    # profile['download.prompt_for_download'] = false
+    # profile['download.default_directory'] = "C:/projetos_ruby/chromedriver.exe"
+    chromedriver_path = File.join(File.absolute_path('../..', File.dirname(__FILE__)),"browsers","chromedriver.exe")
+    Selenium::WebDriver::Chrome.driver_path = chromedriver_path
+    @b = Watir::Browser.new :chrome
+    # @b = Watir::Browser.new :firefox
 
     # Abre a pagina no navegador
     @b.goto 'http://milano.bakecaincontrii.com/be/main.php?page=gestione_annunci_mb'
 
     # localiza o objeto "input" da pagina cujo id e "tabela:j_idt33_filter" (Livro) e preenche com o valor "Isaias"
-    @b.text_field(:name => 'usernamemb').set 'info@dolcerelax.com'
+    @b.text_field(:name => 'usernamemb').set 'franc5555@hotmail.it'
 
     # Aguarda 2 segundos para o filtro ser processado
     # sleep(2)
 
     # localiza o objeto "input" da pagina cujo id e "tabela:j_idt36_filter" (Capitulo) e preenche com o valor "51"
-    @b.text_field(:name => 'passwordmb').set 'maxitaly'
+    @b.text_field(:name => 'passwordmb').set 'vivere'
 
     # Aguarda 2 segundos para o filtro ser processado
     # sleep(2)
@@ -47,41 +55,63 @@ class CH
     
     @b.text_field(:id => 'indirizzopub').set 'rua teste'
     @b.text_field(:id => 'cappub').set '99999'
-    @b.text_field(:id => 'localitapub').set 'KKKKKKKKKKK'
-    
+    @b.text_field(:id => 'localitapub').set 'KKKKKKKKKKK'    
 
     @b.text_field(:id => 'tit').set 'TITOLO'
     @b.text_field(:name => 'eta').set '34'
     @b.text_field(:name => 'contatto').set '34999999999'
     @b.textarea(:id => 'testoann').set 'TEXTO DE EXEMPLO PRO annuncio'
 
-    @b.select_list(:id => 'emailverificate').select 'info@dolcerelax.com'
+    @b.select_list(:id => 'emailverificate').select 'franc5555@hotmail.it'
 
     # @b.label(:text => 'Promuovi il mio annuncio').parent.checkbox :value => 'SITOPLIST'
     @b.checkbox(:value => 'SITOPLIST').set
 
-    sleep(2)
+    sleep(4)
     
     @b.select_list(:id => 'tipo_toplist').select 'Climb the Top 1'
 
-    sleep(2)
+    sleep(3)
 
     @b.select_list(:id => 'num_giorni').select '3 giorni'
 
-    sleep(2)
+    sleep(3)
 
-    @b.select_list(:id => 'fascia_oraria').select '10:00/12:00'    
-    
-    @b.file_field(:id, "upload_id_imgannuncio_doc").set 'C:\Users\Desenvolvimento\Downloads\40440_1502511985.jpg'
+    @b.select_list(:id => 'fascia_oraria').select '10:00/12:00' 
 
-    
-    @b.button(:value => 'CARICA').click
-
-    sleep(5)
+    sleep(3)
 
     @b.checkbox(:name => 'accettopub').set
     
+    @b.button(:value => 'Continua').click
 
+    sleep(2)
+
+    @b.button(:value => 'Continua').click
+
+    sleep(5)
+
+    @b.button(:value => 'GESTIONE IMMAGINI').click
+
+    @b.file_field(:id, "upload_id_imgannuncio_doc").set 'C:\Users\Desenvolvimento\Downloads\40440_1502511985.jpg'
+
+    @b.button(:value => 'CARICA').click
+
+    sleep(2)
+    @b.file_field(:id, "upload_id_imgannuncio_doc").set 'C:\Users\Desenvolvimento\Downloads\11425810_1660007390881429_2557554007159987410_n.jpg'
+
+    @b.button(:value => 'CARICA').click
+
+    sleep(5)
+    @b.button(:value => 'Fine').click
+
+    @b.button(:value => 'GESTIONE IMMAGINI').click
+
+
+
+
+
+    
 
 
     # Localiza a tabela (table) na pagina cujo indice e "0" (a unica tabela que existe na pagina - tabela dos versiculos)
@@ -116,6 +146,11 @@ class CH
     # Fecha o arquivo
     @handle.close
 
+  end
+
+  def action_1
+    # something_to_the_db
+    redirect_to :back
   end
 
 end
